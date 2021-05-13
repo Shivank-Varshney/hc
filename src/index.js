76602f83c4950bbfd3f8ee5fdd6e4f44e9107e29
Shivank Varshney <shivank1703@gmail.com>
2021-05-13 15:58:43 +0530

    sitemap updated

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import App from './App.jsx';
 import './index.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
     <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </BrowserRouter>,
   document.getElementById('root')
 );
